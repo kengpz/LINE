4b14f0a84b9e8a6fe22780c54a00dc3e88563dc8
kengpz <keng_pz@hotmail.com>
2017-05-11 01:42:39 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
 $access_token = 'CUwlk0hS8W7XsdJ0FBpqEufyGplNRYr8y9EdwRqHh7HxzJQu+9PV8WvS5QzrCx2CvD3RXVsLmGKbW/lGt7OtwSDJc+UJqbV76YFasGzy/s6ewHL3CiYMi5aU2VX5VWn+Nxwe5oLTq0kC3EFYY3kZNgdB04t89/1O/w1cDnyilFU=';
-$replyToken = 'C38b5b940c47dade38c04eb701a98208b';
+$roomId = 'C38b5b940c47dade38c04eb701a98208b';
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -15,14 +15,14 @@ if (!is_null($events['events'])) {
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
-			
+			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
 				'text' => $text
 			];
-
+			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
