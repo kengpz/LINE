b44077e5621f3627a6cea6d782a71ddcdffc6ea3
kengpz <keng_pz@hotmail.com>
2017-05-11 00:35:00 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text . $event['events'][0]['source']['userId'];
+				'text' => $text . $replyToken
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
