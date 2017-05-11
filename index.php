e689e22bbeb6447dbf73005a5d3b3bfbaba5fde4
kengpz <keng_pz@hotmail.com>
2017-05-11 21:50:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,41 @@
 <html>
 <body>
 
-<form action="push.php" method="POST">
-Name: <input type="text" name="msg"><br><br>
-<input type="submit">
+<form method="post" name="form">
+<ul><li>
+<input id="msg" name="msg" type="text" />
+</li></ul>
+
+	<div >
+	<input type="submit" value="Submit" class="submit"/>
+	<span class="error" style="display:none"> Please Enter Valid Data</span>
+	<span class="success" style="display:none"> Registration Successfully</span>
+	</div>
 </form>
 
 </body>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.0/jquery.min.js"></script>
+<script type="text/javascript" >
+$(function() {
+$(".submit").click(function() {
+var name = $("#msg").val();
+
+if(name==''){
+	$('.success').fadeOut(200).hide();
+	$('.error').fadeOut(200).show();
+}else {
+	$.ajax({
+	type: "POST",
+	url: "https://muaythaijklsx.herokuapp.com/push.php",
+	data: name,
+	success: function(){
+		$('.success').fadeIn(200).show();
+		$('.error').fadeOut(200).hide();
+		}
+	});
+}
+return false;
+});
+});
+</script>
 </html>
