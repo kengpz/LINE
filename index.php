0ab8c840c22dba508efbc45ddc930bcb6a39b174
kengpz <keng_pz@hotmail.com>
2017-05-11 20:55:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,11 @@
 <!DOCTYPE html>
 <html>
-<head></head>
-<body>   
-<div class="content">
-    <form id="priceForm" name="priceForm" action="push.php" method="POST">
-        <label>Message: </label>
-        <input name="msg" type="text" /><br><br>
+<body>
 
-	<div align="center" style="width:82px; margin-top:2px">
-		<a href="" class="button" id="send" style="width:100%;">
-			<div class="left_border"></div><div class="button_text">Send</div><div class="right_border"></div>
-		</a>
-	</div>
-    </form>
-</div>
+<form action="push.php" method="POST">
+Name: <input type="text" name="msg"><br><br>
+<input type="submit">
+</form>
+
 </body>
-    	<script type="text/javascript" charset="utf-8">
-		$("#send").click(function() {
-			$("#priceForm").submit();
-		});
-	</script>
 </html>
