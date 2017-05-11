5baba58315b7fa0d4c458ea6269ea03c1e0f579e
kengpz <keng_pz@hotmail.com>
2017-05-11 21:05:09 +0700

    Update push.php

--- push.php
+++ push.php
@@ -1,5 +1,5 @@
 <?php
-$msg = $_GET["msg"]; 
+$msg = $_POST["msg"]; 
 $strAccessToken = "CUwlk0hS8W7XsdJ0FBpqEufyGplNRYr8y9EdwRqHh7HxzJQu+9PV8WvS5QzrCx2CvD3RXVsLmGKbW/lGt7OtwSDJc+UJqbV76YFasGzy/s6ewHL3CiYMi5aU2VX5VWn+Nxwe5oLTq0kC3EFYY3kZNgdB04t89/1O/w1cDnyilFU=";
 echo $msg;
 $strUrl = "https://api.line.me/v2/bot/message/push";
@@ -18,7 +18,7 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
 curl_setopt($ch, CURLOPT_HEADER, false);
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
