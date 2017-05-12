e02ec5d0c74e328e92e1f77f4d493caf7c4ffd49
kengpz <keng_pz@hotmail.com>
2017-05-12 11:20:55 +0700

    Update push.php

--- push.php
+++ push.php
@@ -6,7 +6,7 @@ $strAccessToken = "CUwlk0hS8W7XsdJ0FBpqEufyGplNRYr8y9EdwRqHh7HxzJQu+9PV8WvS5QzrC
 if($_POST){
  $msg = $_POST['msg'];
  $corner = $_POST['corner'];
- $msg .= $corner;
+ $msg = $corner.$msg;
 }
 $strUrl = "https://api.line.me/v2/bot/message/push";
  
