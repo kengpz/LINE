d9ccfa41efb3d357b4610e666a330db503bf6c8b
kengpz <keng_pz@hotmail.com>
2017-05-12 14:42:45 +0700

    Create jquery_send.php

---
+++ jquery_send.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Muaythai Demo</title>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+</head>
+<body>
+
+<div id="main">
+<h2>jQuery Ajax $.post() Method</h2>
+<hr>
+
+<form name="sample-form" id="sample-form" action="push.php" method="POST">
+<input type="text" name="msg" id="msg" value="" /><br/>
+<input type="radio" name="corner" value="แดงต่อ" checked> แดง
+<input type="radio" name="corner" value="น้ำเงินต่อ"> น้ำเงิน
+<br>
+<input type="button" name="submit" id="submit" value="Submit" />
+</form><p>
+<div id="result"></div>
+
+</body>
+<script>
+$('#submit').click(function(){
+//validate form
+ var co = getRadioVal("corner");
+ $.post('push.php',$('#sample-form').serialize(),function(response){
+  $('#result').html(response);
+ });
+});
+
+function getRadioVal(radioName) {
+	var rads = document.getElementsByName(radioName);
+	for ( var rad in rads) {
+	 if (rads[rad].checked)
+	  return rads[rad].value;
+	 }
+	return null;
+	}
+</script>
+</html>
