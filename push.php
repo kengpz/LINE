8019fb242be549adfcfca2e28192b8f06976801a
kengpz <keng_pz@hotmail.com>
2017-05-12 09:56:23 +0700

    Update push.php

--- push.php
+++ push.php
@@ -1,21 +1,12 @@
 <?php
 $msg = "default message";
-$corner = "default corner";
-$resultText = "default result";
-$red = "red";
-$blue = "blue";
-$draw = "draw";
-
+$corner = "";
 $strAccessToken = "CUwlk0hS8W7XsdJ0FBpqEufyGplNRYr8y9EdwRqHh7HxzJQu+9PV8WvS5QzrCx2CvD3RXVsLmGKbW/lGt7OtwSDJc+UJqbV76YFasGzy/s6ewHL3CiYMi5aU2VX5VWn+Nxwe5oLTq0kC3EFYY3kZNgdB04t89/1O/w1cDnyilFU=";
 
 if($_POST){
  $msg = $_POST['msg'];
- $corner = $_POST['corner'];
- if(strcmp($corner,$draw) = -1){
-  $resultText = $corner . " ต่อ " . $msg ;
- }else{
-  $resultText = $draw ;
- }
+ $corner = $_POST['msg'];
+ $msg .= $corner;
 }
 $strUrl = "https://api.line.me/v2/bot/message/push";
  
