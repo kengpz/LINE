daef4c413014c2f23cb3352519511608a40e6406
kengpz <keng_pz@hotmail.com>
2017-05-13 20:14:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 				<h3><span class="label label-primary label-lg"><input type="radio" name="corner" value="น้ำเงินต่อ"> น้ำเงิน</span></h3>
 			</div>
 			<div class="col-xs-offset-3 col-xs-2">
-				<h3><span class="label label-primary label-lg"><input type="checkbox" name="draw" value="เสมอ"> เสมอ</span></h3>
+				<h3><span class="label label-info label-lg"><input type="checkbox" name="draw" value="เสมอ"> เสมอ</span></h3>
 			</div>
 		</div>
 		<div class="row">
