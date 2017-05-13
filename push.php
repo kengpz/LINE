69d567edf693f038130fb9d228a7c5ee5e39229d
kengpz <keng_pz@hotmail.com>
2017-05-13 21:58:18 +0700

    Update push.php

--- push.php
+++ push.php
@@ -2,6 +2,7 @@
 $msg = "default message";
 $rate = "";
 $compare = "เสมอ";
+$handicap = "รอง";
 $corner = "";
 $strAccessToken = "CUwlk0hS8W7XsdJ0FBpqEufyGplNRYr8y9EdwRqHh7HxzJQu+9PV8WvS5QzrCx2CvD3RXVsLmGKbW/lGt7OtwSDJc+UJqbV76YFasGzy/s6ewHL3CiYMi5aU2VX5VWn+Nxwe5oLTq0kC3EFYY3kZNgdB04t89/1O/w1cDnyilFU=";
 
@@ -11,6 +12,8 @@ if($_POST){
  $rate = $_POST['rate'];
  if(strcmp($rate, $compare) == 0){
   $msg = $rate . $corner;
+ }else if(strcmp($rate, $handicap) == 0){
+  $msg = $msg . $rate;
  }else {
   $msg = $corner . $rate . $msg;
  }
