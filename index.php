8ab10fa444082b7e483af2e7b68f5abd16b3dcb8
kengpz <keng_pz@hotmail.com>
2017-05-13 16:31:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src="../../dist/js/bootstrap.min.js"></script>
 <title>Muaythai Demo</title>
 </head>
 <body>
