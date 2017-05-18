259abc9998d274ac4ec21619d05eb96e2dc145c9
kengpz <keng_pz@hotmail.com>
2017-05-18 11:21:29 +0700

    Update testiframe.php

--- test/testiframe.php
+++ test/testiframe.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="refresh" content="10">
+<meta http-equiv="refresh" content="">
 <title>Test iframe refresh</title>
 <script>
 window.setInterval("reloadIFrame();", 10000);
