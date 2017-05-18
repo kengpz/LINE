b9fd37ab0fedb52b30d4ac42efa3ea702bfe6504
kengpz <keng_pz@hotmail.com>
2017-05-18 10:51:39 +0700

    Create testiframe.php

---
+++ test/testiframe.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Fouad Ali</title>
+<script>
+var links = "https://www.xn--o3cea3afbwl1da3wf0i.com/poll/open18_muay.php";
+var i = 0;
+var renew = setInterval(function(){
+    document.getElementById("dataframe").src = links;        
+},5000);
+</script>
+</head>
+
+<body>
+<h3>Test iframe</h3>
+<iframe id="dataframe" width="100%" height="100%" frameborder="0" src="https://www.xn--o3cea3afbwl1da3wf0i.com/poll/open18_muay.php" style="width:7300px;height:500px;"></iframe>
+</body>
+</html>
