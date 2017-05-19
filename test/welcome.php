a86456331e15f11a9e7ae2d7c89097ac8345d96c
kengpz <keng_pz@hotmail.com>
2017-05-19 16:19:40 +0700

    Update welcome.php

--- test/welcome.php
+++ test/welcome.php
@@ -2,7 +2,12 @@
 <html>
 <body>
 <?php
-	echo 'xxx';
+if($_POST){
+$signData = hash_hmac('sha1', $_POST['data'],'0JUM9EL2FP5TZ6UXQZQ9EATZJX3D1W2R',false);
+$signData= strtoupper($signData);
+echo ($signData);
+}else {
+	echo "Not post data";
 ?>
 </body>
 </html>
