4505a7121456969bc9ad97d4a3ad8ba9a065dfd8
kengpz <keng_pz@hotmail.com>
2017-12-23 14:59:32 +0700

    Update and rename easy.php to sender.php

---
+++ sender.php
@@ -0,0 +1,35 @@
+<?php
+$msg = '';
+$sat = '';
+$to = '';
+
+if($_POST){
+ $msg = $_POST['msg'];
+ $to = $_POST['to'];
+ $sat= $_POST['sat'];
+}
+
+$strUrl = "https://api.line.me/v2/bot/message/push";
+ 
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$sat}";
+
+$arrPostData = array();
+$arrPostData['to'] = $to;
+$arrPostData['messages'][0]['type'] = "text";
+$arrPostData['messages'][0]['text'] = $msg ;
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$strUrl);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+curl_close ($ch);
+
+?>
