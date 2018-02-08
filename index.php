c644b632e3784fb0ebe40ea1936c9e86038d63ac
kengpz <keng_pz@hotmail.com>
2018-02-08 14:22:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -186,5 +186,26 @@ $( "#p72" ).on( "click", function() {
 });
 </script>
 
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '2119168254979390',
+      cookie     : true,
+      xfbml      : true,
+      version    : '2.12'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 </body>
 </html>
